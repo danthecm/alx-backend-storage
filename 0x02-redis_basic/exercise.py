7eaef7a2341eb857c4df51db69d5d26a5c931e08
danthecm <etchiedaniel1@gmail.com>
2023-10-21 15:56:56 +0100

    0x02: task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,9 +19,26 @@ def count_calls(method: typing.Callable) -> typing.Callable:
 
 
 def call_history(method: typing.Callable) -> typing.Callable:
+    """
+    Count the number of times a method is called and returns the result
+
+    Args:
+        method - the method to call
+
+    Returns:
+        the number of times a method is called
+    """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapper function"""
+        '''
+        Records the method's inputs and output to a list then return output
+
+        Args:
+            args: Arguments
+            kwargs: Key word arguments
+
+        Return: Outputs of the methods called in class Cache
+        '''
         input_key = f"{method.__qualname__}:inputs"
         output_key = f"{method.__qualname__}:outputs"
 
@@ -38,7 +55,42 @@ def call_history(method: typing.Callable) -> typing.Callable:
     return wrapper
 
 
+def replay(fn: typing.Callable) -> None:
+    '''
+    Displays the call history of methods from class Cache
+
+    Args:
+        fn: Function called
+
+    Return: History of inputs and outputs from methods of class Cache
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    stored_redis = getattr(fn.__self__, '_redis', None)
+    if not isinstance(stored_redis, redis.Redis):
+        return
+    func_name = fn.__qualname__
+    input_key = '{}:inputs'.format(func_name)
+    output_key = '{}:outputs'.format(func_name)
+    func_call_count = 0
+    if stored_redis.exists(func_name) != 0:
+        func_call_count = int(stored_redis.get(func_name))
+    print('{} was called {} times:'.format(func_name, func_call_count))
+    func_inputs = stored_redis.lrange(input_key, 0, -1)
+    func_outputs = stored_redis.lrange(output_key, 0, -1)
+    for func_input, func_output in zip(func_inputs, func_outputs):
+        print('{}(*{}) -> {}'.format(
+            func_name,
+            func_input.decode("utf-8"),
+            func_output,
+        ))
+
+
 class Cache:
+    """
+    A class that caches a dictionary of key/value pairs in memory and
+    save them in redis cache.
+    """
     def __init__(self) -> None:
         """Initialize Redis and flush it"""
         self._redis = redis.Redis()
